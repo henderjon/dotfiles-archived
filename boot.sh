cd4770861d7ebc8e836d0ecabdf356311e0249ef
henderjon <list@box370.net>
2016-05-02 08:53:38 -0500

    more fixes

--- boot.sh
+++ boot.sh
@@ -6,17 +6,12 @@
 # install dotfiles
 # git clone $DOTFILES
 
-if [ -z "$1" ]; then
-	printf "please provide a name\n"
+if [ -z "$1" ] || [ -z "$2" ]; then
+	printf "please provide two args: a name for this machine and an email for your ssh key\n"
 	exit 1
 fi
 
-if [ -z "$2" ]; then
-	printf "please provide an email\n"
-	exit 1
-fi
-
-cd
+cd || exit
 # set up working dir
 mkdir -p ~/code/go/bin ~/code/go/pkg ~/code/go/src/github/henderjon
 
@@ -24,14 +19,14 @@ mkdir -p ~/code/go/bin ~/code/go/pkg ~/code/go/src/github/henderjon
 FILES=".gitconfig .screenrc .tmux.conf .vim .vimrc .zshrc"
 for f in $FILES
 do
-	if [ ! -h "$f" ]; then mv ~/"$f" ~/"$f.bkup"; else rm "$f"; fi
+	if [ -e "$f" ] && [ ! -h "$f" ]; then mv ~/"$f" ~/"$f.bkup"; else rm "$f"; fi
 	ln -s ~/dotfiles/"$f" ~/"$f"
 done
 
-sed -i 's/Laptop/$1/' .zshrc
+sed -i "s/Laptop/$1/" .zshrc
 touch ~/.env_zshrc
 
-cd
+cd || exit
 # generate an ssh key
 ssh-keygen -t rsa -C "$2"
 
