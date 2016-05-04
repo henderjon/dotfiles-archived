7b03caefc9e0b065164e30af1ebf60a61be2f7c8
henderjon <list@box370.net>
2016-05-04 10:57:36 -0500

    centos fix

--- sudo.centos.sh
+++ sudo.centos.sh
@@ -68,7 +68,6 @@ cd
 # disable password auth
 sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 sed -i 's/ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/g' /etc/ssh/sshd_config
-sed -i 's/UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
 service sshd reload # systemctl reload sshd.service
 
 cd
